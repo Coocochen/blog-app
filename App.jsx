c3f62b68d30f272859ba78d4284f303cc6e38343
huan <827230613@qq.com>
2019-06-09 02:07:51 +0800

    上传项目

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World!!!<br />
+            To be a better Computer programmer!
+         </div>
+      );
+   }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path=require("path");
+var config = {
+   entry: './main.js',
+   
+   output: {
+      path:path.resolve(__dirname,'./'),
+      filename: 'index.js',
+   },
+   
+   devServer: {
+      inline: true,
+      port: 7777
+   },
+   
+   module: {
+      rules: [ {
+         test: /\.jsx?$/,
+         exclude: /node_modules/,
+         loader: 'babel-loader',
+         
+         query: {
+            presets: ['es2015', 'react']
+         }
+      }]
+   }
+   
+}
+
+module.exports = config;
